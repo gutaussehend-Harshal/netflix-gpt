7b878589cafb4ca1fef366f7c6207d4ac263b866
Harshal Amrut Patil <harshal.patil@gmail.com>
2025-09-28 14:43:22 +0530

    Added Sign In/ Sign Up form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 flex items-center justify-center text-3xl h-screen">
+          Hello World!!!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
